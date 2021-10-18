c09e2da4630f82eb14d7836f5ac0e3ec90885fe4
Mariya Koshelenko <k.mary.srg@gmail.com>
2021-10-18 20:20:51 +0300

    Added open/close side menu

---
+++ scripts/sidebar-menu.js
@@ -0,0 +1,11 @@
+const openBtn = document.querySelector(".sidebar__hamburger");
+const closedBtn = document.querySelector(".sidebar__close");
+const sidebar = document.querySelector('.sidebar');
+
+openBtn.addEventListener("click", function (){
+  sidebar.classList.add('sidebar--opened');
+});
+
+closedBtn.addEventListener('click', function (){
+  sidebar.classList.remove('sidebar--opened');
+});
