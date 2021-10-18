100eaa5446ce946920f5dea75431175e2cbdf61b
Mariya Koshelenko <k.mary.srg@gmail.com>
2021-10-19 01:24:05 +0300

    Added sorting

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,13 +1,27 @@
 (function() {
-  const controlls = document.querySelectorAll('.product__link');
-  const activeClass = 'product__item--active';
+  const elem = document.querySelector('.products__list');
+  const iso = new Isotope(elem, {
+    itemSelector: '.products__item',
+    filter: '.popular'
+  });
+
+  const controlls = document.querySelectorAll('.filter__link');
+  const activeClass = 'filter__item--active';
+
   controlls.forEach(function(control) {
     control.addEventListener('click', function(e) {
       e.preventDefault();
+
+      const filterName = control.getAttribute("data-filter");//метод getAttribute позволяет получить значение атрибута data-filter
+        console.log(filterName);
       controlls.forEach(function(link) {
-        link.closest('.product__item').classList.remove(activeClass);
+        link.closest('.filter__item').classList.remove(activeClass);
       })
-      control.closest('.product__item').classList.add(activeClass); //метод .closest ищет родительский класс filter__item переданного элемента и добавляет к нему activeClass
+      control.closest('.filter__item').classList.add(activeClass); //метод .closest ищет родительский класс filter__item переданного элемента и добавляет к нему activeClass
+
+      iso.arrange({
+        filter: `.${filterName}`
+      })
     })
   })
 }());
