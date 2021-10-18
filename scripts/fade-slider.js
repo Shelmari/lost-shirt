f1d5c8f65ed52d1674a07e640e572dbdd4f0b2a5
Mariya Koshelenko <k.mary.srg@gmail.com>
2021-10-18 21:39:41 +0300

    Added animation for the slider

---
+++ scripts/fade-slider.js
@@ -0,0 +1,12 @@
+(function () {
+  const sliders = document.querySelectorAll(".fade-slider__item");
+  const activeClass =  "fade-slider__item--visible";
+  let index = 0;
+  setInterval(function() {
+    sliders[index].classList.remove(activeClass);
+    index++;
+    if (index + 1 > sliders.length)
+      index = 0;
+    sliders[index].classList.add(activeClass);
+  }, 5000);
+}());
